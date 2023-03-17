80297b0a4a2afc1a475aff384819dbb0c1a25956
j-rachel <124406958+j-rachel@users.noreply.github.com>
2023-03-17 16:06:59 -0700

    Update week9lab.py

--- week9lab.py
+++ week9lab.py
@@ -1,21 +1,39 @@
 import RPi.GPIO as GPIO
 import datetime
- 
-def my_callback(channel):
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now())) 
- 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
- 
-    message = raw_input('\nPress any key to exit.\n')
- 
-finally:
-    GPIO.cleanup()
+import time
 
+channel = 2
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
 
-    
+f = open("data5.csv","w")
+meta_data = ["CPM","Time Tags"]
+import csv
+f = open("data5.csv","w",newline='')
+writer = csv.writer(f)
+writer.writerow(meta_data)
+
+startTime=int(time.time())
+iTime=startTime
+count = 0
+list_of_times = []
+originalStart = int(time.time())
+
+while iTime < (originalStart + 140):
+ temp = GPIO.wait_for_edge(channel, GPIO.FALLING, timeout = 1000)
+ if temp is None:
+  print("Timeout")
+ else:
+  count = count + 1
+ list_of_times.append(str(datetime.datetime.now()))
+ iTime = int(time.time())
+ if iTime > (startTime + 60):
+  data = [str(count),str(list_of_times)]
+  writer.writerow(data)
+  print("Counts in the last minute: " + str(count))
+  count = 0
+  list_of_times = []
+  startTime = int(time.time())
+ 
+print("Total Counts:" + str(count))
+f.close()
